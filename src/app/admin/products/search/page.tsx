4f1193de4d9ab94c50ad107d3288ab8a2cdb10b9
Jennifer CHileno Santisteban <jennifer15aries@gmail.com>
2025-05-07 23:53:40 -0500

    feat : funcionalidad de la busqueda

--- src/app/admin/products/search/page.tsx
+++ src/app/admin/products/search/page.tsx
@@ -1,5 +1,8 @@
+import { ProductSearch } from '@/components/product/ProductSearch';
+import ProductTable from '@/components/product/ProductTable';
 import { Heading } from '@/components/ui/heading/Heading'
 import { prisma } from '@/lib/prisma';
+import { Link } from 'lucide-react';
 import React from 'react'
 
 async function searchProducts(search : string){
@@ -11,6 +14,9 @@ async function searchProducts(search : string){
         //para buscar sin importar mayusculas o minusculas
         mode : 'insensitive'
       }
+    },
+    include : {
+      category : true
     }
   })
 
@@ -19,13 +25,28 @@ async function searchProducts(search : string){
 
 export default async function SearchPage ({searchParams} : {searchParams : {search : string}}) {
 
-  searchProducts(searchParams.search);
+  const products = await searchProducts(searchParams.search);
 
   return (
     <>
       <Heading>
-        Resultados de busqueda
+        Resultados de busqueda {searchParams.search}
       </Heading>
+
+      <div className='flex flex-col lg:flex-row lg:justify-end mt-5'>
+        <ProductSearch/>
+      </div>
+
+      {products.length ? (
+        <ProductTable
+          products={products}
+        />
+      ):(
+        <div className='flex justify-center items-center mt-5'>
+          <h1 className='text-2xl font-bold text-gray-500'>No se encontraron productos</h1>
+        </div>
+      )}
+      
     </>
   )
 }
